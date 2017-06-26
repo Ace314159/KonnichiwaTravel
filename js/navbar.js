26a15db4614d22825ba5e4cd89ff9928f2e8cf67
Akash <akash.munagala@gmail.com>
2017-06-27 00:22:04 +0530

    Sync Translations Page With Firebase

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,17 @@
 $(window).ready(function() {
-	$("#select-place").dropdown();
+	$("#select-place").dropdown({
+		onChange: placeChange
+	});
+
+
+	var database = firebase.database();
+	var places = database.ref("Places");
+
+	places.on("value", function(snapshot) {
+		snapshot.forEach(function(country) {
+			country.forEach(function(city) {
+				$("#select-place").append("<option value='" + country.key + "'>" + city.val() + "</option");
+			});
+		});
+	});
 });
--- js/translations.js
+++ js/translations.js
@@ -1,3 +1,56 @@
+var languages;
+var data;
+
 $(window).ready(function() {
-	$("#select-category").dropdown();
+	$("#language").dropdown({
+		onChange: search
+	});
+	$("#category").dropdown({
+		onChange: search
+	});
+	$("#phrase").on("input", function() {
+		if($(".select-place .selected").html() !== undefined) {
+			search();
+		}
+	});
+
+
+	var database = firebase.database();
+	data = database.ref("Translations/Data");
+	languages = database.ref("Translations/Languages");
 });
+
+function search() {
+	$("#results").html("");
+	data.once("value").then(function(snapshot) {
+		snapshot.forEach(function(category) {
+			$("#category .menu").append("<div class='item'>" + category.key +"</div>");
+			$("#results").append("<div><p class='category-name'>" + category.key +"</p></div>");
+			category.forEach(function(translations) {
+				if(translations.key == $("#language .text").html()) {
+					translations.forEach(function(translation) {
+						$("#results").append("<div class='translation'><p class='original'>" 
+							+ translation.key +"</p><p class='translated'>" 
+							+ translation.val() + "</p></div>");
+					});
+				}
+			});
+		});
+	});
+}
+
+function placeChange(value, text) {
+	languages.once("value").then(function(snapshot) {
+		snapshot.forEach(function(country) {
+			if(country.key === value) {
+				country.forEach(function(language) {
+					if(language.key.split("~")[1] === "Default") {
+						$("#language .default.text").html(language.val());
+					}
+					$("#language .menu").append("<div class='item'>" + language.val() +"</div>");
+				})
+			}
+		});
+	});
+	search();
+}
