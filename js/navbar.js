93fc18381dc8f998b764d7b50f5532ce462f3b8f
Unknown <ykumar849@gmail.com>
2017-06-30 09:56:42 +0530

    Work on Login Page

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
 	var database = firebase.database();
 	var places = database.ref("Places");
+	var provider = new firebase.auth.GoogleAuthProvider();
 
 	places.on("value", function(snapshot) {
 		$("#select-place .default.text").html("Where do you want to go?");
@@ -13,5 +14,28 @@ $(document).ready(function() {
 		});
 		$("#select-place").removeClass("disabled");
 		$("#select-place").dropdown('refresh');
+	});
+
+	$("#login").click(function() {
+			firebase.auth().signInWithPopup(provider).then(function(result) {
+	      // This gives you a Google Access Token. You can use it to access the Google API.
+	      var token = result.credential.accessToken;
+	      console.log("signin done : " + token);
+
+	      // The signed-in user info.
+	      var user = result.user;
+	      // ...
+	    }).catch(function(error) {
+	      // Handle Errors here.
+	      var errorCode = error.code;
+	      var errorMessage = error.message;
+	      // The email of the user's account used.
+	      var email = error.email;
+	      // The firebase.auth.AuthCredential type that was used.
+	      var credential = error.credential;
+	      console.log("Error : " + errorMessage);
+
+	      // ...
+	    });
 	});
 });
