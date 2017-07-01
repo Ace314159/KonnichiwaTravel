29823ca77f05fd6d1f2339e5fa392ab0f7e424e7
Rajiv Seelam <rajiv@betalectic.com>
2017-07-01 12:06:13 +0530

    Trying to enable cors on php

--- ola.php
+++ ola.php
@@ -1,6 +1,6 @@
 <?php 
 
-header("Access-Control-Allow-Origin: *");
+
 
 
 $opts = array(
@@ -21,8 +21,11 @@ $url = $_GET['url'].'?'.http_build_query($params);
 
 $file = file_get_contents($url, false, $context);
 
-
-
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 echo $file;
 
 ?>
